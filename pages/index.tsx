b654efb188fa1f420a4c592d1e7cf9a993221c15
Jamirul Islam <johndoe@example.com>
2022-11-01 18:06:25 +0530

    project-structure-created

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import LayoutMain from "../components/Layouts/LayoutMain";
+
+
+
+export default function Home() {
+  return (
+      <LayoutMain title={"Home | E-Cart"}>
+          <div>
+            home
+          </div>
+      </LayoutMain>
+  )
+}
